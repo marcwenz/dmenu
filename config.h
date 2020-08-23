eaff323d25ddb4bbc964ac4a13949c6b37f165f3
Marc Wenzlawski <llbbbmn.wenzlawski@gmail.com>
2020-08-23 13:23:44 +0200

    initial commit

--- config.h
+++ config.h
@@ -2,7 +2,7 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
+static int fuzzy = 1;                       /*  -F option; if 0, dmenu doesn't use fuzzy matching */
 static int centered = 0;                    /* -c option; centers dmenu on screen */
 static int min_width = 500;                    /* minimum width when centered */
 
@@ -15,11 +15,11 @@ static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
 	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeSelHighlight] = { "#ffc978", "#005577" },
-	[SchemeNormHighlight] = { "#ffc978", "#222222" },
+	[SchemeSelHighlight] = { "#fcba03", "#005577" },
+	[SchemeNormHighlight] = { "#fcba03", "#222222" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
-/* -l and -g options; controls number of lines and columns in grid if > 0 */
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
 static unsigned int lineheight = 0;         /* -h option; minimum height of a menu line     */
 static unsigned int columns    = 0;
